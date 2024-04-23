df7f6ce4932fc58daef642e9422e77084787cc69
Najmul <najmulbinnurul@gmail.com>
2024-04-23 21:40:10 +0600

    [B] app.ts and server.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,23 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import config from './config';
+import { apiNotFound } from './utils/API-not-found';
 
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`<p>{App_NAME} server running on PORT ${config.port}</p>
+  <a href="${config.frontend_url}">visit frontend</a>
+  `);
+});
+
+// middleware
+// app.use(globalErrorhandler);
+
+app.use(apiNotFound);
+
+export default app;
---
+++ backend/src/server.ts
@@ -0,0 +1,41 @@
+/* eslint-disable no-console */
+import { Server } from 'http';
+import config from './config';
+import app from './app';
+
+let server: Server;
+
+process.on('uncaughtException', error => {
+  console.log(error);
+  process.exit(1);
+});
+
+async function bootstrap() {
+  try {
+    console.log('🚀 Database connected succesfully');
+    server = app.listen(config.port, () => {
+      console.log(`🚀 Application listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log('❌ Failed to connect database', error);
+  }
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        console.log(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+
+bootstrap();
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM is received');
+  if (server) {
+    server.close();
+  }
+});
